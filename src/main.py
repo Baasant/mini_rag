17afa7051fb76633e6d050eb6786fc7c98fec71a
Baasant <41596266+Baasant@users.noreply.github.com>
2025-12-23 16:38:19 +0200

    organize the repo

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
 load_dotenv(".env")
-from  mini_rag.routes import base
+from  routes import base
 
 app = FastAPI()
 app.include_router(base.base_router)
---
+++ src/routes/base.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI ,APIRouter,Depends
+import os
+from helpers.config import get_settings
+base_router = APIRouter(
+    prefix="/api/v1",
+    # tag name 
+    tags=["api_v1"]
+)
+@base_router.get("/")
+async def welcome(app_settings=Depends(get_settings)):
+    # app_setting=get_settings()
+    app_name=app_settings.APP_NAME
+
+    app_version=app_settings.APP_VERSION
+    return {
+        "app_name": app_name,
+        "app_version":app_version,
+    } 
